f90dd846de529d4ed6bc777e5d457fe279e0648d
小寺　隆聖 <kotera.ryusei@jp,fujitsu.com>
2022-03-01 14:04:31 +0900

    問題１完了

---
+++ kakezan.js
@@ -0,0 +1,32 @@
+/**
+ *  メイン処理
+ *  lines: Array<string> 入力された行(末尾は必ず改行)分の配列
+ */
+ const main = () => {
+
+    const N1 = lines[0]
+    const N2 = lines[1]
+    N1N2 = N1 * N2 
+
+    // 空行
+    console.log(N1N2)
+}
+
+/**
+ * 標準入出力のための処理
+ */
+const reader = require('readline')
+
+process.stdin.resume()
+process.stdin.setEncoding('utf8')
+const rl = reader.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+let lines = []
+rl.on('line', (line) => {
+    lines.push(line)
+})
+
+rl.on('close', main)
