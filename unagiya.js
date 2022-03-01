9901ce9be3e7e00438108cfd939d6bc303bd3890
小寺　隆聖 <kotera.ryusei@jp,fujitsu.com>
2022-03-01 16:31:50 +0900

    うなぎ屋できません

--- unagiya.js
+++ unagiya.js
@@ -2,25 +2,38 @@
  *  メイン処理
  *  lines: Array<string> 入力された行(末尾は必ず改行)分の配列
  */
- const main = () => {
+const main = () => {
 
-    let count = 0
     const N1 = lines[0].split(' ')
-    const numberOfSeats = N1[0]
-    const numberOfGroups = N1[1]
-    let seatArray = new Array( n );
+    const numberOfSeats = Number(N1[0])
+    const numberOfGroups = Number(N1[1])
+    const seatArray = new Array(numberOfSeats).fill(1)
+    let count = 0
 
-    for(i=1 ,i < numberOfGroups+2 ,i++){   
-        seatTag = lines[i][1]
-        numberOfTeammates = lines[i][0]
-        for (seatTag, seatTag<=seatTag+numberOfTeammates, seatTag++){
-            if(seatArray[seatTag])
+    for (i = 1; i < (numberOfGroups + 2); i++) {
+        const tempLine = lines[i].split(' ')
+        console.log(tempLine)
+        let numberOfTeammates = Number(tempLine[0])
+        let seatTag = Number(tempLine[1])
+        let tempCount = 0
+        for (j = seatTag; j < (seatTag + numberOfTeammates); j++) {
+            let seatTagFinal = j % numberOfSeats
+            console.log(seatTagFinal)
+            if (seatArray[seatTagFinal] == 1) {
+                tempCount += 1
+            }
+            if (tempCount == numberOfTeammates) {
+                seatArray.fill(2, seatTag-1, seatTagFinal-1)
+                count += numberOfTeammates
+            }
         }
     }
 
+    console.log(count)
+
 }
 
-
+console.log("start")
 /**
  * 標準入出力のための処理
  */
